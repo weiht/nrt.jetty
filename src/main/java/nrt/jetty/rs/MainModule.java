a7322c4d178ae12f45944da5eb246bd32409e043
haitaowei <haitaowei@hyperats.com>
2015-01-22 15:00:03 +0800

    IocBy annotation added to rsFilter's main module.

--- src/main/java/nrt/jetty/rs/MainModule.java
+++ src/main/java/nrt/jetty/rs/MainModule.java
@@ -1,8 +1,14 @@
 package nrt.jetty.rs;
 
+import nrt.jetty.web.NrtJsonIocProvider;
+
+import org.nutz.mvc.annotation.IocBy;
 import org.nutz.mvc.annotation.Modules;
 
 @Modules(packages={"org.necros.rs.modules", "nrt.jetty.rs.modules"})
+@IocBy(type=NrtJsonIocProvider.class, args={
+	"NUTZ-MVC"
+})
 public class MainModule {
 
 }
